7d92a1bd79bdccb14ef1eeeb9f78229a70cb04f5
Robert Obreczarek <robert@obreczarek.com>
2016-11-09 22:19:30 +0100

    rough version of nav is working
    TODO
    [ ] stop main nav item from shifting during hover action
    [ ] test on other browsers

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,5 +5,6 @@
     }
 
 
+    return (root.Menu = Menu);
 
 }(this));
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,14 +1,14 @@
-(function () {
+(function (root) {
 
     'use strict';
 
     function bootstrap() {
 
         var debug = true;
-        var menu = new Menu(debug);
+        var menu = new root.Menu(debug);
 
     }
 
     window.addEventListener('load', bootstrap);
 
-}());
+}(this));
